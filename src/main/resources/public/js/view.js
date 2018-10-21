6061fd6cf54d846c9fdd67ff08ed49b2c5c1f1a9
JiangLin296 <jianglin296@gmail.com>
2018-10-20 20:31:51 -0500

    I have changed all function based on the master branch

--- src/main/resources/public/js/view.js
+++ src/main/resources/public/js/view.js
@@ -24,66 +24,71 @@ function createApp(canvas) {
     var c = canvas.getContext("2d");
 
     //draw a dot
-    var drawDot = function(x, y, radius, color) {
-        c.fillStyle = color;
+    var drawDot = function(x, y, size) {
+        c.fillStyle = "rgb(0, 0, 0)";
         c.beginPath();
-        c.arc(x, y, radius, 0, 2 * Math.PI, false);
+        c.arc(x, y, size/2, 0, 2 * Math.PI, false);
         c.closePath();
         c.fill();
     };
     //draw a wall
-    var drawWall = function(startX, startY) {
+    var drawWall = function(startX, startY,size) {
         c.fillStyle="black";
         c.beginPath();
         c.moveTo(startX,startY);
-        c.lineTo(startX+10,startY);
-        c.lineTo(startX+10,startY+10);
-        c.lineTo(startX,startY+10);
+        c.lineTo(startX+size,startY);
+        c.lineTo(startX+size,startY+size);
+        c.lineTo(startX,startY+size);
         c.closePath();
         c.fill();
         c.stroke();
     };
     //draw a exit
-    var drawExit=function (startX,startY) {
+    var drawExit=function (startX,startY,size) {
         c.fillStyle="white";
         c.beginPath();
         c.moveTo(startX, startY);
-        c.lineTo(startX, startY + 10);
+        c.lineTo(startX, startY + size);
         c.stroke();
     };
     //draw a image
-    var drawImage=function (shape) {
+    var drawImage=function (x,y,size,path) {
         var image=new Image();
-        image.src=location.href + shape.path;
+        image.src=location.href + path;
         c.save();
-        c.translate(shape.loc.x+5,shape.loc.y+5);
+        c.translate(x+size/2,y+size/2);
         // c.scale(shape.collisionIndex,1);
         // c.rotate(shape.angle);
-        c.drawImage(image,-5,-5,10,10);
+        c.drawImage(image,-size/2,-size/2,size,size);
         c.restore();
     };
-    //draw a Pacman
-    var drawPacman = function(x, y, radius, color) {
+    //draw a pacman
+    var drawPacMan = function(x, y, size) {
         var mouth=Math.abs(frameCount%90-45);
         c.beginPath();
-        c.arc(x, y, radius, mouth/180 * Math.PI, (mouth/180+1)* Math.PI, false);
+        c.arc(x, y, size/2, mouth/180 * Math.PI, (mouth/180+1)* Math.PI, false);
         c.fillStyle = "rgb(255, 255, 0)";
         c.fill();
         c.beginPath();
-        c.arc(x, y, radius, -(mouth/180+1) * Math.PI, -(mouth/180) * Math.PI, false);
+        c.arc(x, y, size/2, -(mouth/180+1) * Math.PI, -(mouth/180) * Math.PI, false);
         c.fill();
         // c.beginPath();
-        // c.arc(x, y-25, 10, 0, 2 * Math.PI, false);
+        // c.arc(x, y-6, 2, 0, 2 * Math.PI, false);
         // c.fillStyle = "rgb(0, 0, 0)";
         // c.fill();
+    };
+    //clear
+    var clear = function() {
+        c.clearRect(0,0, canvas.width, canvas.height);
     };
     
     return {
         drawExit:drawExit,
         drawDot: drawDot,
         drawWall: drawWall,
-        drawPacman:drawPacman,
+        drawPacMan:drawPacMan,
         drawImage:drawImage,
+        clear:clear,
         dims: {height: canvas.height, width: canvas.width}
     }
 }
@@ -92,8 +97,8 @@ function createApp(canvas) {
  * Entry point into app
  */
 window.onload = function() {
-    reset();
     app = createApp(document.querySelector("canvas"));
+    reset();
     canvasDims();
     setInterval(updateGameWorld, 100);
 }
@@ -106,26 +111,34 @@ function initGame() {
     $.get("/resetGame", function (data, status) {
         var pObs = data.obs;
         pObs.forEach(function(element) {
-            if (element.type === "Wall"){
-                app.drawWall(element.loc.x,element.loc.y)
+            if (element.type === "wall"){
+                app.drawWall(element.location.x,element.location.y)
             }
-            else if(element.type === "Exit") {
-                app.drawExit(element.loc.x, element.loc.y);
+            else if(element.type === "exit") {
+                app.drawExit(element.location.x, element.location.y);
             }
-            else if(element.type==="Pacman"){
-                app.drawPacman(element.loc.x,element.loc.y,element.radius,element.color)
+            else if(element.type==="pacman"){
+                app.drawPacMan(element.location.x,element.location.y,element.size)
             }
-            else if(element.type==="Ghost"){
-                app.drawImage(element)
+            else if(element.type==="ghost"){
+                if(element.color="red"){
+                    app.drawImage(element.location.x,element.location.y,element.size,"reaGhostPath")
+                }
+                else if (element.color="pink"){
+                    app.drawImage(element.location.x,element.location.y,element.size,"pinkGhostPath")
+                }
+                else if (element.color="orange"){
+                    app.drawImage(element.location.x,element.location.y,element.size,"orangeGhostPath")
+                }
+                else if (element.color="blue"){
+                    app.drawImage(element.location.x,element.location.y,element.size,"blueGhostPath")
+                }
             }
-            else if(element.type==="SmallDot"){
-                app.drawDot(element.loc.x,element.loc.y,element.radius,element.color)
+            else if(element.type==="small_dot"){
+                app.drawDot(element.location.x,element.location.y,element.size)
             }
-            else if(element.type==="BigDot"){
-                app.drawDot(element.loc.x,element.loc.y,element.radius,element.color)
-            }
-            else if(element.type==="Fruit"){
-                app.drawImage(element)
+            else if(element.type==="big_dot"){
+                app.drawDot(element.location.x,element.location.y,element.size)
             }
         });
     }, "json");
@@ -140,26 +153,37 @@ function updateGameWorld() {
         frameCount=frameCount+10;
         var pObs = data.obs;
         pObs.forEach(function(element) {
-            if (element.type === "Wall"){
-                app.drawWall(element.loc.x,element.loc.y)
+            if (element.type === "wall"){
+                app.drawWall(element.location.x,element.location.y)
             }
-            else if(element.type === "Exit") {
-                app.drawExit(element.loc.x, element.loc.y);
+            else if(element.type === "exit") {
+                app.drawExit(element.location.x, element.location.y);
             }
-            else if(element.type==="Pacman"){
-                app.drawPacman(element.loc.x,element.loc.y,element.radius,element.color)
+            else if(element.type==="pacman"){
+                app.drawPacMan(element.location.x,element.location.y,element.size)
             }
-            else if(element.type==="Ghost"){
-                app.drawImage(element)
+            else if(element.type==="ghost"){
+                if(element.color="red"){
+                    app.drawImage(element.location.x,element.location.y,element.size,"reaGhostPath")
+                }
+                else if (element.color="pink"){
+                    app.drawImage(element.location.x,element.location.y,element.size,"pinkGhostPath")
+                }
+                else if (element.color="orange"){
+                    app.drawImage(element.location.x,element.location.y,element.size,"orangeGhostPath")
+                }
+                else if (element.color="blue"){
+                    app.drawImage(element.location.x,element.location.y,element.size,"blueGhostPath")
+                }
             }
-            else if(element.type==="SmallDot"){
-                app.drawDot(element.loc.x,element.loc.y,element.radius,element.color)
+            else if(element.type==="small_dot"){
+                app.drawDot(element.location.x,element.location.y,element.size)
             }
-            else if(element.type==="BigDot"){
-                app.drawDot(element.loc.x,element.loc.y,element.radius,element.color)
+            else if(element.type==="big_dot"){
+                app.drawDot(element.location.x,element.location.y,element.size)
             }
-            else if(element.type==="Fruit"){
-                app.drawImage(element)
+            else if(element.type==="fruit"){
+                app.drawImage(element.location.x,element.location.y,element.size,"fruitPath")
             }
         });
     }, "json");
@@ -186,6 +210,6 @@ function reset() {
  * switch the direction
  */
 function switchDirection(keycode) {
-    $.post("/switchDirection", {keycode:keycode }, function (data, status) {
+    $.post("/switchDirection", {keycode:keycode}, function (data, status) {
     }, "json");
 }
