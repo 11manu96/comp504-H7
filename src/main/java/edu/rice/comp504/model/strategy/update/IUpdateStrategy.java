15bbcc79c9b9b5f649ca2beef9ddb3546c39a997
Jonathan Wang <jonathanwang017@berkeley.edu>
2018-10-22 08:05:58 -0500

    Move uml diagram

--- src/main/java/edu/rice/comp504/model/strategy/update/IUpdateStrategy.java
+++ src/main/java/edu/rice/comp504/model/strategy/update/IUpdateStrategy.java
@@ -1,21 +1,21 @@
 package edu.rice.comp504.model.strategy.update;
 
-import edu.rice.comp504.model.gameObjects.AGameObject;
+import edu.rice.comp504.model.gameobjects.AGameObject;
 
 /**
- * This interface is used to add an object update behavior
+ * This interface is used for object update behavior strategies.
  */
 public interface IUpdateStrategy  {
 
     /**
-     * Get the name of the strategy
+     * Get the name of the strategy.
      * @return The strategy name
      */
     public String getName();
 
     /**
-     * update the state of the object
+     * Update the state of the object.
      * @param context The object to update
      */
-    public void Update(AGameObject context);
+    public void update(AGameObject context);
 }
