d11d48f7c29ee990e1dbcc80c6edafcdc65360c5
Jonathan Wang <jonathanwang017@berkeley.edu>
2018-10-22 08:21:25 -0500

    Re-add iupdatestrategy interface

---
+++ src/main/java/edu/rice/comp504/model/strategy/update/IUpdateStrategy.java
@@ -0,0 +1,20 @@
+package edu.rice.comp504.model.strategy.update;
+
+import edu.rice.comp504.model.gameobjects.AGameObject;
+
+/**
+ * This interface is used for game object updat behavior.
+ */
+public interface IUpdateStrategy {
+    /**
+     * Get the interaction update name.
+     * @return The interaction update name
+     */
+    public String getName();
+
+    /**
+     * Update the state of a game object.
+     * @param context game object to update
+     */
+    public void update(AGameObject context);
+}
